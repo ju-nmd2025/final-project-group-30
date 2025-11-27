cd693d24319e41545a316f909b9c3249c11dbaa6
eliseekblad <ekel24ve@student.ju.se>
2025-11-27 09:42:46 +0100

    grunder

--- game.js
+++ game.js
@@ -1,42 +1,52 @@
-import { character } from "./character";
-import platform from "platform";
+/*import { character } from "./character";
+import platform from "platform"; */
 
-function setup() {
-    createCanvas(400, 400);
+
+let x = 100 ;
+let y = 100 ;
+
+function setup (){
+    createCanvas (400,400);
+    
 }
 
-// Obstacle / Spike / Death
-function drawObstacle() {
+function draw(){
+    background(0,200,250);
+
+    x--;
+    if (x<0){ 
+    x=400;
+}
+
+drawCharacter();
+drawPlatform(100, 300);
+drawPlatform2(300,250);
+}
+
+function drawCharacter(){
+    //character
     push();
-    fill("red");
-    triangle(180, 300, 210, 240, 240, 300);
+    fill("magenta");
+    rect(x-50, y+150, 50, 50);
     pop();
 }
 
-let x = 100;
-let y = 100;
-
-function draw() {
-    background(100, 100, 100);
-
-    character.draw();
-	platform.draw();
-
-    platform.x -= 10;
-    if(platform.x + platform.w < 0){
-        platform.x = 500;
-    }
-
-    if(character.y + character.h < 300){
-        character.y += 10;
-    }
-
-    // Floor
-    line(0, 300, 400, 300);
+function drawPlatform(x, y){
+    //platform
+    push();
+    fill("pink");
+    rect(x-40, y-120, 80, 20);
+    pop();
 }
+function drawPlatform2(x, y){
+//platform2
+push();
+fill("pink");
+rect(x-40, y-120, 80, 20);
+pop();
 
-function keyPressed(){
-    if(character.y + character.h === 300){
-        character.y -= 80;
-    }
+
+//floor
+line(0, 300, 400, 300);
+
 }
